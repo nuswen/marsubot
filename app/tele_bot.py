4c17c8d6797c3152e2f1918f6e6c36bacf14b75e
nuswen <donn.nus@gmail.com>
2019-06-21 11:38:37 +0300

    init

--- app/tele_bot.py
+++ app/tele_bot.py
@@ -23,7 +23,7 @@ def hi_msg(msg):
     else:
         productFileId = 0
 
-    poster(bot, msg.chat.id, len(command)+msg.text)
+    poster(bot, msg.chat.id, str(len(command))+msg.text)
 
 @bot.message_handler(content_types=['photo'])
 def photo(msg):
