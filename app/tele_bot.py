fdcc0f07ce9b24e5c1323c6a6fcb8b29b5b0c81b
nuswen <donn.nus@gmail.com>
2019-06-21 11:16:54 +0300

    init

--- app/tele_bot.py
+++ app/tele_bot.py
@@ -19,11 +19,11 @@ def hi_msg(msg):
     if command == 'dwn': # команда на скачивание продукта
         productId = int(msg.text[10:])
         productData = models.Product.filter_by(Id=productId).first()
-        productFileId = productData.FileId
+        productFileId = productData.FileId()
     else:
         productFileId = 0
 
-    poster(bot, msg.chat.id, command == 'dwn')
+    poster(bot, msg.chat.id, command)
 
 @bot.message_handler(content_types=['photo'])
 def photo(msg):
