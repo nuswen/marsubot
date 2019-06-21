3c4c172b3fb67676fe973c3c36b11e592991e7e5
nuswen <donn.nus@gmail.com>
2019-06-21 13:47:39 +0300

    init

--- tools.py
+++ tools.py
@@ -8,7 +8,7 @@ def msg_dwn_new_usr(productId):
     productData = models.product.query.filter_by(Id = productId).first()
     msgId = productData.MessageId
     msgDate = models.messages.query.filter_by(Id = msgId).first()
-    url = productData.ProductLink
+    url = productData.DownloadLink
     name = productData.ProductName
-    text = msgDate.Text % (name + ' ' + url)
+    text = msgDate.Text % (name + '/n/n' + url + '/n/n')
     return text
