c360092c6585aa01794be3d5ad706607e8a367b3
nuswen <donn.nus@gmail.com>
2019-06-21 14:05:17 +0300

    init

--- tele_bot_tools.py
+++ tele_bot_tools.py
@@ -6,7 +6,7 @@ def poster(bot, chat_id, text, buttons=None, ed=False, message_id=None, doc=None
         if ed:
             bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=text, reply_markup=keyboarder(buttons))
         else:
-            bot.send_message(chat_id, text, , reply_markup=keyboarder(buttons))
+            bot.send_message(chat_id, text, reply_markup=keyboarder(buttons))
     else:
         if ed:
             bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=text)
