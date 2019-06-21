55ca14a94bf04fd6b4b04b935697b8946b0337fc
nuswen <donn.nus@gmail.com>
2019-06-21 12:19:33 +0300

    init

--- app/tele_bot.py
+++ app/tele_bot.py
@@ -18,8 +18,8 @@ def hi_msg(msg):
 
     if command == 'dwn': # команда на скачивание продукта
         productId = int(msg.text[10:])
-        productData = models.Messages.query.filter_by(Id = 1).first()
-        #productFileId = productData.FileId()
+        productData = models.Product.query.filter_by(MessageId = 1).first()
+        productFileId = productData.FileId()
     else:
         productFileId = 0
 
