e993c7d2e75035696cbb8753a16ac31ebed52293
nuswen <donn.nus@gmail.com>
2019-06-24 16:04:56 +0300

    init

--- app/tele_bot.py
+++ app/tele_bot.py
@@ -57,4 +57,6 @@ def any_messages(msg):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
-    pass
+    textDate, buttons = menu_builder(call)
+    poster(bot, msg.chat.id, textDate.Text, addTag=textDate.TagAdd, 
+    remTag=textDate.TagRem, buttons=buttons, doc=textDate.Attach)
