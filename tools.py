61627badf7a938b26ea9f7a6ac1d92e0c748eb29
nuswen <donn.nus@gmail.com>
2019-07-17 23:05:05 +0300

    init

--- tools.py
+++ tools.py
@@ -47,8 +47,10 @@ def menu_builder(call, user = None):
             waitUser = models.waitlist.query.filter_by(Id = user).first()
             if waitUser:
                 db.session.delete(waitUser)
-                db.session.commit()
-        newUser = models.waitlist(Id = user, WhatWait = 'text', From = 'hi message newbie')
+        callSave = str(call)
+        callSave = callSave[:1]
+        newUser = models.waitlist(Id = user, WhatWait = 'text', From = 'hi message newbie', 
+        Call = callSave)
         db.session.add(newUser)
         db.session.commit()
             
