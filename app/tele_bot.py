4ca02fd59acd302852ffa020f1fa3d1c0630554e
nuswen <donn.nus@gmail.com>
2019-07-18 14:57:35 +0300

    init

--- app/tele_bot.py
+++ app/tele_bot.py
@@ -57,16 +57,17 @@ def smenu(msg):
     
     if textDate[:-1] != []:
         for i in textDate[:-1]:
-          poster(bot, msg.chat.id, i.Text, addTag=i.TagAdd, 
-          remTag=i.TagRem, doc=i.Attach, img = i.Img)
+            poster(bot, msg.message.chat.id, i.Text, addTag=i.TagAdd, 
+            remTag=i.TagRem, doc=i.Attach, img = i.Img)
 
-        poster(bot, msg.message.chat.id, textDate[-1:].Text, addTag=textDate[-1:].TagAdd, 
-          remTag=textDate[-1:].TagRem, buttons=buttons, doc=textDate[-1:].Attach, 
-          img = textDate[-1:].Img)
+        i = textDate[-1:]
+        poster(bot, msg.message.chat.id, i.Text, addTag=i.TagAdd, 
+        remTag=i.TagRem, buttons=buttons, doc=i.Attach, img = i.Img)
     else:
-        poster(bot, msg.chat.id, textDate[0].Text, addTag=textDate[0].TagAdd, 
-          remTag=textDate[0].TagRem, buttons=buttons, doc=textDate[0].Attach, \
-              img = textDate[0].Img, ed = True, message_id=msg.message_id)
+        i = textDate[0]
+        poster(bot, msg.message.chat.id, i.Text, addTag=i.TagAdd, 
+        remTag=i.TagRem, buttons=buttons, doc=i.Attach,
+        img = i.Img, ed = True, message_id=msg.message.message_id)
 
 @bot.message_handler(content_types=['photo'])
 def photo(msg):
@@ -94,13 +95,15 @@ def callback_inline(call):
     
     if textDate[:-1] != []:
         for i in textDate[:-1]:
-          poster(bot, call.message.chat.id, i.Text, addTag=i.TagAdd, 
-          remTag=i.TagRem, doc=i.Attach, img = i.Img)
+            poster(bot, call.message.chat.id, i.Text, addTag=i.TagAdd, 
+            remTag=i.TagRem, doc=i.Attach, img = i.Img)
 
-        poster(bot, call.message.chat.id, textDate[-1:].Text, addTag=textDate[-1:].TagAdd, 
-          remTag=textDate[-1:].TagRem, buttons=buttons, doc=textDate[-1:].Attach, 
-          img = textDate[-1:].Img)
+        i = textDate[-1:]
+        poster(bot, call.message.chat.id, i.Text, addTag=i.TagAdd, 
+          remTag=i.TagRem, buttons=buttons, doc=i.Attach, 
+          img = i.Img)
     else:
-        poster(bot, call.message.chat.id, textDate[0].Text, addTag=textDate[0].TagAdd, 
-          remTag=textDate[0].TagRem, buttons=buttons, doc=textDate[0].Attach, \
-              img = textDate[0].Img, ed = True, message_id=call.message.message_id)
+        i=textDate[0]
+        poster(bot, call.message.chat.id, i.Text, addTag=i.TagAdd, 
+        remTag=i.TagRem, buttons=buttons, doc=i.Attach,
+        img = i.Img, ed = True, message_id=call.message.message_id)
