05676d8c83f067ef0a356ea64cba41f6f35176eb
nuswen <donn.nus@gmail.com>
2020-06-11 14:28:40 +0300

    init

--- app/__init__.py
+++ app/__init__.py
@@ -44,14 +44,14 @@ def verify():
 def handle_messages():
     data = request.get_json()
     entry = data['entry'][0]
+    print(data)
 
-    if entry.get("messaging"):
-        messaging_event = entry['messaging'][0]
-        sender_id = messaging_event['sender']['id']
-        print(messaging_event)
-        message_text = 'goodbye'
-        # message_text = messaging_event['message']['text']
-        send_text_message(sender_id, message_text)
+    # if entry.get("messaging"):
+    #     messaging_event = entry['messaging'][0]
+    #     sender_id = messaging_event['sender']['id']
+    #     print(messaging_event)
+    #     message_text = messaging_event['message']['text']
+    #     send_text_message(sender_id, message_text)
     return 'ok', 200
 
 def send_text_message(recipient_id, message):
